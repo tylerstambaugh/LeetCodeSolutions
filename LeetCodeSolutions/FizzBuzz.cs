a7baf87c94eff6fa7a6bd2a493c2a4b14873d85e
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-03-05 20:10:39 -0500

    Add project files.

---
+++ LeetCodeSolutions/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions
+{
+    public static class FizzBuzz
+    {
+        public static IList<string> FizzBuzzer(int n)
+        {
+            List<string> returnList = new List<string>();
+            string returnString = "";
+            for (int i = 1; i <= n; i++)
+            {
+                returnString = 
+                    (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" :
+                    i % 3 == 0 ? "Fizz" :
+                    i % 5 == 0 ? "Buzz" :
+                    i.ToString();
+
+                returnList.Add(returnString);
+            }
+
+            return returnList;
+        }
+    }
+}
---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LeetCodeSolutions;
+
+var result = FizzBuzz.FizzBuzzer(15);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}   
+
+Console.ReadLine();
