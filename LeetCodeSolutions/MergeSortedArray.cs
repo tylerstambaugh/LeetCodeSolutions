e4afa1e11c669f845c19a44c3bfaa5f0f1b6a299
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-03-06 21:29:36 -0500

    randoms

---
+++ LeetCodeSolutions/MergeSortedArray.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeSolutions
+{
+    public class MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (nums1[i] > nums2[i])
+                {
+                    nums1[i] = nums2[i];
+                }
+            }
+        }
+    }
+}
---
+++ LeetCodeSolutions/NumberOfSteps.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions
+{
+    public static class NumberOfSteps
+    {
+        public static int Solution(int num)
+        {
+            int steps = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                }
+                else
+                {
+                    num -= 1;
+                }
+                steps++;
+            }
+            return steps;
+        }
+    }
+}
