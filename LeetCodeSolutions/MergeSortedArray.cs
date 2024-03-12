cd4ec6c5bd4e14aa8901e3836bbd5ea6b807e2f3
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-03-11 21:20:03 -0400

    works on 4 / 59 cases

--- LeetCodeSolutions/MergeSortedArray.cs
+++ LeetCodeSolutions/MergeSortedArray.cs
@@ -6,25 +6,31 @@
         {           
             int leftCounter = 0, rightCounter = 0;
 
-            for (int i = 1; i < nums1.Length; i++)
+            if (nums2.Length > 0)
             {
-                Console.WriteLine($"iteration = {i}");
-                if (nums1[leftCounter] <= nums2[rightCounter])
+                for (int i = 1; i <= nums1.Length; i++)
                 {
-                    Console.WriteLine($"nums1[{leftCounter}] = {nums1[leftCounter]}");
-                    Console.WriteLine($"nums2[{rightCounter}] = {nums2[rightCounter]}");
-                    Console.WriteLine("nothing changes, incrementing left counter");
-                    leftCounter++;
-                }
-                else
-                {
-                    Console.WriteLine($"nums1[{leftCounter}] = {nums1[leftCounter]}");
-                    Console.WriteLine($"nums2[{rightCounter}] = {nums2[rightCounter]}");
-                    Console.WriteLine("Right side is smaller, taking value at right array and putting it back into sorted array. Incrementing both counters.");
-                    nums1[leftCounter + 1] = nums1[leftCounter];
-                    nums1[leftCounter] = nums2[rightCounter];
-                    rightCounter++;
-                    leftCounter++;
+                    Console.WriteLine($"iteration = {i}");
+                    if (nums1[leftCounter] != 0 && nums1[leftCounter] <= nums2[rightCounter])
+                    {
+                        Console.WriteLine($"nums1[{leftCounter}] = {nums1[leftCounter]}");
+                        Console.WriteLine($"nums2[{rightCounter}] = {nums2[rightCounter]}");
+                        Console.WriteLine("nothing changes, incrementing left counter");
+                        leftCounter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"nums1[{leftCounter}] = {nums1[leftCounter]}");
+                        Console.WriteLine($"nums2[{rightCounter}] = {nums2[rightCounter]}");
+                        Console.WriteLine("Right side is smaller, taking value at right array and putting it back into sorted array. Incrementing both counters.");
+                        if (leftCounter < nums1.Length - 1)
+                        {
+                            nums1[leftCounter + 1] = nums1[leftCounter];
+                        }
+                        nums1[leftCounter] = nums2[rightCounter];
+                        rightCounter++;
+                        leftCounter++;
+                    }
                 }
             }
         }
