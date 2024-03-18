9ef6d29a6c1adb605e5af4352b13d038fbd5d487
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2024-03-18 15:01:52 -0400

    mergeSortedArray passing

--- LeetCodeSolutions/MergeSortedArray.cs
+++ LeetCodeSolutions/MergeSortedArray.cs
@@ -3,7 +3,7 @@
     public class MergeSortedArray
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
-        {           
+        {
             int leftCounter = 0, rightCounter = 0;
 
             if (nums2.Length > 0)
@@ -36,15 +36,15 @@
         }
 
         public void Merge2(int[] nums1, int m, int[] nums2, int n)
-        {            
-            int nums1Index = 0, nums2Index = 0; 
+        {
+            int nums1Index = 0, nums2Index = 0;
 
-            if(m == 0 && n == 1)
+            if (m == 0 && n == 1)
             {
                 nums1[0] = nums2[0];
             }
 
-            if(m == 1 && n == 1)
+            if (m == 1 && n == 1)
             {
                 if (nums1[0] > nums2[0])
                 {
@@ -63,7 +63,7 @@
 
                 while (nums1Index < nums1.Length && nums2Index < n)
                 {
-                    if (nums1[nums1Index] > nums2[nums2Index] || nums1[nums1Index] == 0)
+                    if (nums1[nums1Index] > nums2[nums2Index] && nums1Index < m)
                     {
                         //shift remaining values of nums1 to the right
                         for (int i = nums1.Length - 1; i >= nums1Index + 1; i--)
@@ -76,6 +76,13 @@
 
                         nums1Index++;
                         nums2Index++;
+
+                    }
+                    else if (nums1Index >= m)
+                    {
+                        nums1[nums1Index] = nums2[nums2Index];
+                        nums1Index++;
+                        nums2Index++;
                     }
                     else
                     {
@@ -83,6 +90,64 @@
                     }
                 }
             }
-        }    
+        }
+
+        public void Merge3(int[] nums1, int m, int[] nums2, int n)
+        {
+
+            int nums1Index = 0, nums2Index = 0;
+
+            while (nums1Index < nums1.Length)
+            {
+                //if number on left is equal or bigger
+                if (nums2[nums2Index] >= nums1[nums1Index])
+                {
+                    //shift everthing right from end to nums1 index
+                    for (int i = nums1.Length - 1; i > nums1Index; i--)
+                    {
+                        nums1[i] = nums1[i - 1];
+                    }
+
+                    //swap number from nums2 into nums1
+                    nums1[nums1Index + 1] = nums2[nums2Index];
+
+                }
+                nums2Index++;
+                nums1Index++;
+            }
+        }
+
+        public void Merge4(int[] nums1, int m, int[] nums2, int n)
+        {
+            //only compare if the nums2[nums2index] is smaller than nums1[nums1index] up to (m + iteration),
+            //if it is, shift nums1[index + 1] to right and put nums2[num2index] to nums1index + 1
+            //if it is not, add it to end of nums1 (nums1[m + iteration] = nums2[nums2index])
+
+            int iteration = 0;
+
+            for (int nums2Index = 0; nums2Index < n; nums2Index++)
+            {
+                for (int nums1Index = 0; nums1Index < nums1.Length; nums1Index++)
+                {
+                    if (nums2[nums2Index] < nums1[nums1Index])
+                    {
+                        for (int i = nums1.Length - 1; i > nums1Index; i--)
+                        {
+                            nums1[i] = nums1[i - 1];
+                        }
+                        nums1[nums1Index] = nums2[nums2Index];
+                        iteration++;
+                        break;
+                    }
+                    else if (nums1Index == m + iteration)
+                    {
+                        nums1[m + iteration] = nums2[nums2Index];
+                        iteration++;
+                        break;
+                    }
+                }
+            }
+        }
+
     }
 }
